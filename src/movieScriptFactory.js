3e4814b5f3ebb0c76119aadd92b385622a57d28f
Norman-w <85535885@qq.com>
2021-09-18 08:36:08 +0800

    初始化

---
+++ src/movieScriptFactory.js
@@ -0,0 +1,28 @@
+export default class MovieScriptFactory {
+  //region 创建视频结构体信息
+  CreateMovie(param= {id:'',name:'',desc:'',duration:10.5,usedFor:'',url:'',extType:''})
+  {
+    return param;
+  }
+  //endregion
+  //region 为视频创建锚点信息
+  CreateAnchorInformation4Video(param=
+                                  {
+                                    movie: {}, name: '',
+                                    desc: '',
+                                    reWriteSubTitle: '',
+                                    reWriteVoice: '',
+                                    start: 0.00,
+                                    end: 0.00,
+                                    duration: 0.00,
+                                    id: ''
+                                  }) {
+    let ret =
+      {
+        ...param,
+        id: new Date().getDate()
+      };
+    return ret;
+  }
+  //endregion
+}
