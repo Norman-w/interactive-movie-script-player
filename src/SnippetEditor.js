49425892c1e739da09d856abc9acd736674f1c2e
Norman-w <85535885@qq.com>
2021-09-20 17:23:04 +0800

    看SnippetEditor.js中的相关注释

--- src/SnippetEditor.js
+++ src/SnippetEditor.js
@@ -2,7 +2,11 @@ import React, {Component} from 'react';
 import classNames from './SnippetEditor.module.css'
 import {Input, Select} from "antd";
 const {Option} = Select;
-
+/*2021年09月20日17:23:01
+已经实现了视频可以循环播放,接下来添加节点的时候,让节点可以选择播放完毕以后的动作
+                                                                             问题类 播放完毕后播放等待视频等待交互,回答类 播放完毕后跳转到目标视频, 等待类 播放完毕后重复播放 中间有交互后跳出视频播放转到回答类逻辑
+snippetEditor可以继续进行细化了.各个片段的相关的信息 如时常,所属视频 所属脚本 开始时间结束时间 如果是问题类型可以选择现有的循环片段等.
+* */
 class SnippetEditor extends Component {
     state=
         {
