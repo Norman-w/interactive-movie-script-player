eadb0713f60be786d9b25dab186c473b1a57489d
Norman-w <85535885@qq.com>
2021-10-14 11:52:45 +0800

    时钟控件已经允许拖拽选时间.下一步是使用数字输入控制时间

--- src/QPScriptInteractors/TimeSelector.js
+++ src/QPScriptInteractors/TimeSelector.js
@@ -1,52 +1,41 @@
 import React, {useRef, useState} from 'react';
-import classes from './TimeSelector.module.css'
-function getAngle(cen, first, second) {
-  // cen  : 中心点 [0,0]<br>　　　　 // first : 开始点 [1,3]<br>　　　　 // second : 结束位置 [3,4]
-  console.log('获取角度入参:',cen,first,second)
-  if (!cen || cen.length!==2 || !first || first.length!==2 || !second || second.length !==2)
-  {
-    return null;
-  }
-  var f_c_x = first[0] - cen[0],
-    f_c_y = cen[1] - first[1],
-    s_c_x = second[0] - cen[0],
-    s_c_y = cen[1] - second[1];
-  var c = Math.sqrt(f_c_x * f_c_x + f_c_y * f_c_y) * Math.sqrt(s_c_x * s_c_x + s_c_y * s_c_y);
-  if (c == 0) return -1;
-  var angle = Math.acos((f_c_x * s_c_x + f_c_y * s_c_y) / c);
-  // 第一象限
-  if (cen[0] - second[0] < 0 && cen[1] - second[1] < 0) {
-    return angle
-    // 第二象限
-  } else if (cen[0] - second[0] < 0 && cen[1] - second[1] > 0) {
-    return angle
-    // 第三象限
-  } else if (cen[0] - second[0] > 0 && cen[1] - second[1] < 0) {
-    return 2 * Math.PI - angle
-    // 第四象限
-  } else if (cen[0] - second[0] > 0 && cen[1] - second[1] > 0) {
-    return 2 * Math.PI - angle
-  }
-}
+import {findDOMNode} from 'react-dom';
 
+
+import classes from './TimeSelector.module.css'
+var _constValue=
+    {
+        R2D : 180 / Math.PI,
+    }
 function TimeSelector(props) {
   const [hour,setHour] = useState(3);
   const [minute,setMinute] = useState(21);
-  const [mouseDownPos, setMouseDownPos] = useState([]);
+  // const [isDragging, setIsDragging] =useState(false);
+    const [draggingPointer, setDraggingPointer] =useState(null);
+  const [mouseDownAngle, setMouseDownAngle]=useState(0);
+  const [mouseMoveAngle, setMouseMoveAngle]=useState(0);
   const [mouseCurrentPos, setMouseCurrentPos] = useState([]);
+  const [center, setCenter] = useState(null);
   const centerDom = useRef(null);
   let centerSize = 20;
   let clockSize = 200;
   let centerLocation = [750,360];
-  // if (centerDom)
-  // {
-  //   console.log(centerDom)
-  //   centerLocation=  [centerDom.offsetWidth,centerDom.offsetHeight];
-  // }
-  let first=mouseDownPos;
-  let second = mouseCurrentPos;
-  let angle = getAngle(centerLocation,first,second);
-  console.log('当前角度:', angle);
+  //region 获取表的中心
+    if (!center)
+    {
+        var c = document.getElementById('centerDom')
+        // console.log(c)
+        var centerPoint = findDOMNode(c);
+        if(centerPoint)
+        {
+            // console.log(centerPoint);
+            let bound = centerPoint.getBoundingClientRect();
+            setCenter( [bound.left+bound.width/2, bound.top + bound.height/2])
+        }
+    }
+
+    //endregion
+  // console.log('当前角度:', angle);
   //region 表盘
   let clockStyle={
     width: clockSize +2,
@@ -149,6 +138,19 @@ function TimeSelector(props) {
   let hourPointerLong = clockSize/2/2;
   let hourPointerOffsetX = (clockSize-hourPointerWidth)/2;
   let hourPointerOffsetY = (clockSize-hourPointerLong)/2;
+  let angle = 0;
+  if (draggingPointer === 'hour')
+  {
+      angle = mouseMoveAngle;
+  }
+  else if(draggingPointer === 'minute')
+  {
+      angle = mouseMoveAngle/12 + (parseInt(''+hour)*30);
+  }
+  else
+  {
+      angle = hour*30;
+  }
   let hourStyle =
     {
       borderRadius:hourPointerWidth/2,
@@ -158,7 +160,7 @@ function TimeSelector(props) {
       width:hourPointerWidth,
       height:hourPointerLong,
       backgroundColor:'#cfeaa1',
-      transform:'rotateZ('+ (hour*30+minute*0.5) + 'deg) translateY(-'+((hourPointerLong/2)-hourPointerLong*0.1)+'px)' ,
+      transform:'rotateZ('+ angle + 'deg) translateY(-'+((hourPointerLong/2)-hourPointerLong*0.1)+'px)' ,
       // transform:'rotateZ('+ (angle) + 'deg) translateY(-'+((hourPointerLong/2)-hourPointerLong*0.1)+'px)' ,
     }
   //endregion
@@ -167,6 +169,18 @@ function TimeSelector(props) {
   let minutePointerLong = clockSize*0.4;
   let minutePointerOffsetX = (clockSize-minutePointerWidth)/2;
   let minutePointerOffsetY = (clockSize-minutePointerLong)/2;
+  let minuteAngle = 0;
+  if(draggingPointer === 'hour')
+  {
+      minuteAngle = mouseMoveAngle%30/30*360
+  }
+  else if(draggingPointer === 'minute')
+  {
+      minuteAngle = mouseMoveAngle;
+  }
+  else {
+      minuteAngle = minute * 6
+  }
   let minuteStyle =
     {
       borderRadius:minutePointerWidth/2,
@@ -176,16 +190,72 @@ function TimeSelector(props) {
       width:minutePointerWidth,
       height:minutePointerLong,
       backgroundColor:'#7bc0db',
-      transform:'rotateZ('+ minute*6 + 'deg) translateY(-'+((minutePointerLong/2)-minutePointerLong*0.1)+'px)' ,
+      transform:'rotateZ('+ minuteAngle + 'deg) translateY(-'+((minutePointerLong/2)-minutePointerLong*0.1)+'px)' ,
     }
   //endregion
+    //region 点下鼠标的时候记录点的角度
+    const handleMouseDown = (e)=>
+    {
+        console.log('按下',draggingPointer)
+        if (!draggingPointer)
+        {
+            return;
+        }
+        console.log('按下')
+        var x = e.clientX - center[0];
+        var y = e.clientY - center[1];
+        var startAngle = _constValue.R2D * Math.atan2(x, -y);
+        if (startAngle<0)
+        {
+            startAngle=180-(0-startAngle) + 180;
+        }
+        console.log('按下角度:',startAngle)
+        setMouseDownAngle(startAngle);
+        // setIsDragging(true);
+    }
+    //endregion
+    //region 鼠标移动的时候记录角度
+    const handleMouseMove =(e)=>
+    {
+        if (draggingPointer==='hour' || draggingPointer === 'minute')
+        {
+            var x = e.clientX - center[0];
+            var y = e.clientY - center[1];
+            var currentAngle = _constValue.R2D * Math.atan2(x, -y);
+            let currentAngle360 = currentAngle;
+            if (currentAngle<0)
+            {
+                currentAngle360=180-(0-currentAngle) + 180;
+            }
+            setMouseMoveAngle(currentAngle360);
+        }
+    }
+    //endregion
   return (
     <div className={classes.container}
-         onMouseDown={event => {setMouseDownPos([event.clientX,event.clientY])
-           console.log(event)
+         // onMouseDown={(event)=>{handleMouseDown(event)}}
+         onMouseMove={event=>handleMouseMove(event)}
+         onMouseUp={event=>{
+             if (draggingPointer === 'hour')
+             {
+                 console.log('新时间:',mouseMoveAngle/30)
+                 setHour(mouseMoveAngle/30);
+                 let newMinute = (mouseMoveAngle%30)/30*60;
+                 console.log('新分钟',newMinute)
+                 setMinute(newMinute);
+             }
+             else if(draggingPointer === 'minute')
+             {
+                 let newMinute = (mouseMoveAngle/6);
+                 setMinute(newMinute);
+                 setHour(parseInt(''+hour)+newMinute/60);
+             }
+
+             setMouseDownAngle(0);
+             // setIsDragging(false);
+             setDraggingPointer(null);
+             setMouseMoveAngle(0) ;
          }}
-         onMouseUp={event => setMouseDownPos([])}
-         onMouseMove={event => setMouseCurrentPos([event.clientX,event.clientY])}
     >
       <div className={classes.main} style={clockStyle}>
 
@@ -193,11 +263,18 @@ function TimeSelector(props) {
         {fiveMDoms}
         {oneMDoms}
         <div className={classes.pointer} style={hourStyle}
-
+             onMouseDown={event => setDraggingPointer('hour')}
+             onMouseUp={event=>setDraggingPointer(null)}
         ></div>
-        <div className={classes.pointer} style={minuteStyle}></div>
-        <div className={classes.centerRed} style={centerRedStyle} ref={centerDom}></div>
-        <div>当前角度{angle} 按下位置{mouseDownPos} 移动中位置:{mouseCurrentPos}</div>
+        <div className={classes.pointer} style={minuteStyle}
+             onMouseDown={event => setDraggingPointer('minute')}
+             onMouseUp={event=>setDraggingPointer(null)}
+        ></div>
+        <div className={classes.centerRed} style={centerRedStyle} ref={centerDom} id={'centerDom'}></div>
+        <div> 按下位置:{mouseDownAngle}</div>
+          <div>移动中位置:{mouseMoveAngle}</div>
+          <div>角度差:{mouseMoveAngle-mouseDownAngle}</div>
+          <div>点击的指针:{draggingPointer}</div>
       </div>
     </div>
   );
