09221b326d433ae4e744e66b5f4565a00185fcf0
Norman-w <85535885@qq.com>
2021-10-16 17:41:31 +0800

    已经完成了上下午的设置等.下一步进行点击时间对时间的设置,如果需要增强效果,可以拉动鼠标进行时间的增减.上和右方向是加,下和左方向是减.为了避免上和左,右和下的切换,需要在点击以后 向哪个方向移动指定的距离之后才判断是加还是减.如果是向上右向左,不能直接切换,应该在打到了向上的条件的时候,设置当前操作为向上.然后再进行距离的测算,对步长进行转换时间.但是这里面需要有一个速度和加速度的概念.所以比较难.简单点就是需要直接点击时间 进行其他时间的选择或者是点击时间以后进行输入.

--- src/QPScriptInteractors/TimeSelector.js
+++ src/QPScriptInteractors/TimeSelector.js
@@ -17,80 +17,7 @@ var _constValue=
       center:null,
       round:0,//圈数   圈数0 是从0点起的第一圈,圈数 1 是下午的那一圈. 下午那一圈的0点是12点.
     };
-    function getTimeByAngle(byPointer,mouseMoveAngle)
-    {
-      let currentHour = save.hour;
-      let currentMinute = save.minute;
-      let draggingPointer = byPointer;
-      let oldTime = save.lastUpdateTime;
-      if (draggingPointer === 'hour') {
-        currentHour = Math.floor(mouseMoveAngle / 30);
-        currentMinute = Math.round((mouseMoveAngle % 30) * 2);
-      } else if (draggingPointer === 'minute') {
-        currentMinute = Math.round(mouseMoveAngle / 6);
-      }
-      //region 59.5-60秒 都会四舍五入到60,这时应该让60做为0展示
-      if (currentMinute === 60) {
-        currentMinute = 0;
-        // let round = Math.round(mouseMoveAngle/30);
-        // currentHour = round + currentHour;
-        // console.log('正好60分的时候  hour是:', currentHour, 'round:',round);
-      }
-      //endregion
-      //region 如果是往新的小时方向移动的话 增减小时
-      if (save.lastUpdateTime.minute>45&&save.lastUpdateTime.minute<60 && currentMinute>=0&& currentMinute<15)
-      {
-        //向右侧移动;
-        console.log('需要增加时间,当前早时间:', save.hour);
-        let newHour = (save.hour + 1)% 12;
-        if (newHour<0)
-        {
-          newHour = 12+newHour;
-        }
-        currentHour = newHour;
-        save.hour = newHour;
-        console.log('需要增加时间,当前', newHour)
-      }
-      else if(save.lastUpdateTime.minute>=0 && save.lastUpdateTime.minute <15 && currentMinute>45 && currentMinute<60)
-      {
-        //向右侧移动;
-        let newHour = (save.hour -1 )% 12;
-        if (newHour<0)
-        {
-          newHour = 12+newHour;
-        }
-        currentHour = newHour;
-        save.hour = newHour;
-      }
-      //endregion
-      let currentRound = save.round;
-      if(oldTime.hour<12 && oldTime.hour>=9 && (currentHour >=0 || currentHour === 12) && currentHour <=3)
-      {
-        //增加时间
-        currentRound = Math.abs(currentRound+1)%2;
-      }
-      else if((oldTime.hour>=0 || oldTime ===12) && oldTime<=3 && currentHour<12 && currentHour>=9)
-      {
-        currentRound = Math.abs(currentRound-1)%2;
-        console.log('要减少时间了.');
-      }
-      let newTime =  {hour:currentHour,minute:currentMinute,round:currentRound};
-      // console.log('新的时间是:',newTime);
-      return newTime;
-    }
-//region 点下鼠标的时候记录点的角度
-const handleMouseDown = (e)=>
-{
-  console.log('按下',save.draggingPointer)
-  if (!save.draggingPointer)
-  {
-    return;
-  }
-  // let downAngle = getAngleByClientPoint(e.clientX,e.clientY);
-  //   setMouseDownAngle(downAngle);
-  save.moving=false;
-}
-//endregion
+
 //region 鼠标移动的时候记录角度
 const handleMouseMove =(e)=>
 {
@@ -98,11 +25,11 @@ const handleMouseMove =(e)=>
   {
     return false;
   }
-  // console.log('移动:',save.draggingPointer);
   if (save.draggingPointer==='hour' || save.draggingPointer === 'minute')
   {
     save.moving=true;
 
+    //region 计算鼠标角度
     var x = e.clientX - save.center[0];
     var y = e.clientY - save.center[1];
     var currentAngle = _constValue.R2D * Math.atan2(x, -y);
@@ -111,35 +38,68 @@ const handleMouseMove =(e)=>
     {
       currentAngle360=180-(0-currentAngle) + 180;
     }
-    // console.log('角度:', currentAngle);
-    let needUpdate = setMouseMoveAngle(currentAngle360);
-    return needUpdate;
+    //endregion
+      //region 根据指针名称和角度 计算时间
+      let oldTime = save.lastUpdateTime;
+      if (save.draggingPointer === 'hour') {
+          save.hour = Math.floor(currentAngle360 / 30);
+          save.minute = Math.round((currentAngle360 % 30) * 2);
+      } else if (save.draggingPointer === 'minute') {
+          save.minute = Math.round(currentAngle360 / 6);
+      }
+      //region 59.5-60秒 都会四舍五入到60,这时应该让60做为0展示
+      if (save.minute === 60) {
+          save.minute = 0;
+          if(save.draggingPointer === 'hour')
+          {
+              save.hour = (save.hour+1)%12;
+          }
+      }
+      //endregion
+      //region 如果是分针往新的小时方向移动的话 增减小时
+      if (save.draggingPointer === 'minute') {
+          if (save.lastUpdateTime.minute > 45 && save.lastUpdateTime.minute < 60 && save.minute >= 0 && save.minute < 15) {
+              //向右侧移动;
+              // console.log('需要增加时间,当前早时间:', save.hour);
+              let newHour = (save.hour + 1) % 12;
+              if (newHour < 0) {
+                  newHour = 12 + newHour;
+              }
+              save.hour = newHour;
+              // console.log('需要增加时间,当前', newHour)
+          } else if (save.lastUpdateTime.minute >= 0 && save.lastUpdateTime.minute < 15 && save.minute > 45 && save.minute < 60) {
+              //向右侧移动;
+              let newHour = (save.hour - 1) % 12;
+              if (newHour < 0) {
+                  newHour = 12 + newHour;
+              }
+              save.hour = newHour;
+          }
+      }
+      //endregion
+      if(oldTime.hour!==save.hour && oldTime.hour<12 && oldTime.hour>=9 && save.hour >=0 && save.hour <=3)
+      {
+          //增加时间
+          save.round = Math.abs(save.round+1)%2;
+          // console.log('需要更新上下午.',save.round);
+      }
+      else if(oldTime.hour!==save.hour &&  oldTime.hour>=0 && oldTime.hour<=3 && save.hour<12 && save.hour>=9)
+      {
+          save.round = Math.abs(save.round-1)%2;
+          // console.log('需要更新上下午.',save.round);
+      }
+      let newTime =  {hour:save.hour,minute:save.minute,round:save.round};
+      if (newTime.minute === oldTime.minute)
+      {
+          return null;
+      }
+      // console.log('新时间:',newTime,'老时间',oldTime)
+      //endregion
+      return newTime
   }
   return false;
 }
 //endregion
-const setMouseMoveAngle = function (e)
-{
-  if (save.moving) {
-    let time = getTimeByAngle(save.draggingPointer,e);
-    if (save.lastUpdateTime.minute!== time.currentMinute)
-    {
-      save.hour = time.hour;
-      save.minute = time.minute;
-      save.round = time.round;
-      // console.log('setMouseMoveAngle 需要更新时间呢:', time)
-      return true;
-    }
-    else
-    {
-      return false;
-    }
-  }
-  else {
-    save.mouseMoveAngle = e;
-    return false;
-  }
-}
 
 const setDraggingPointer = function (e)
 {
@@ -156,14 +116,10 @@ function TimeSelector(props) {
   let clockSize = 200;
   //endregion
   //region state 和函数
-  // const [hour,setHour] = useState(3);
-  // const [minute,setMinute] = useState(21);
-  //   const [draggingPointer, setDraggingPointer] =useState(null);
-  // const [mouseDownAngle, setMouseDownAngle]=useState(0);
-  // const [mouseMoveAngle, setMouseMoveAngle]=useState(0);
 
   let propTime = props.time ? props.time : {hour: 0, minute: 0, round: 0};
   const [time, setTime] = useState(propTime);
+  const [round, setRound] = useState(save.round);
   const center = save.center;
   useEffect(() => {
     if (!center) {
@@ -187,9 +143,6 @@ function TimeSelector(props) {
   //endregion
 
   //region 获取表的中心
-  console.log('中心您是:', center);
-
-
   //endregion
   //region 表盘和中心
   //region 表盘
@@ -345,39 +298,33 @@ function TimeSelector(props) {
       transform: 'rotateZ(' + minuteAngle + 'deg) translateY(-' + ((minutePointerLong / 2) - minutePointerLong * 0.1) + 'px)',
     }
 
+  //是不是中午
+  let isNoon = time.hour ===0 && round ===1;
+  let hourText = ('' + (isNoon ? 12:time.hour)).padStart(2, '0');
 
   return (
     <div className={classes.container}
          onMouseDown={(event) => {
-           handleMouseDown(event)
+             if (!save.draggingPointer)
+             {
+                 return;
+             }
+             save.moving=false;
          }}
          onMouseMove={event => {
-           let needUpd = handleMouseMove(event);
-           if (needUpd) {
-             // console.log('需要更新时间,接收到的时间是:', save.hour,save.minute)
-             let newHour = save.hour;
-             let newMinute = save.minute;
-             let newRound = save.round;
-             save.lastUpdateTime.hour = newHour * 1;
-             save.lastUpdateTime.minute = newMinute * 1;
-             save.lastUpdateTime.round = newRound * 1;
-             // setHour(newHour);
-             // setMinute(newMinute);
-             setTime({hour: newHour, minute: newMinute, round: newRound});
+           let newTime = handleMouseMove(event);
+           if (newTime) {
+             setTime(newTime);
+             if (newTime.round!== save.lastUpdateTime.round) {
+                 setRound(newTime.round);
+             }
+             save.lastUpdateTime = newTime;
            }
          }}
          onMouseUp={event => {
            if (save.draggingPointer) {
-             save.hour = currentHour;
-             save.minute = currentMinute;
-             save.moving = false;
-             // setHour(currentHour);
-             // setMinute(currentMinute)
-
-             // setMouseDownAngle(0);
-             // setIsDragging(false);
-             setDraggingPointer(null);
-             setMouseMoveAngle(0);
+               save.draggingPointer = null;
+               save.moving = false;
            }
          }}
     >
@@ -419,15 +366,23 @@ function TimeSelector(props) {
         </div>
       </div>
       <div className={classes.timeTextEditor}>
-        <div className={classes.hour}>{('' + time.hour).padStart(2, '0')}</div>
+        <div className={classes.hour}>{hourText}</div>
         <div className={classes.maoHao}>
           <div className={classes.maoHao1}></div>
           <div className={classes.maoHao2}></div>
         </div>
         <div className={classes.minute}>{('' + time.minute).padStart(2, '0')}</div>
         <div className={classes.rounds}>
-          <div className={time.round === 0 ? classes.roundSelected : classes.round}>上午</div>
-          <div className={time.round === 1 ? classes.roundSelected : classes.round}>下午</div>
+          <div className={round=== 0 ? classes.roundSelected : classes.round}
+               onClick={event => {
+                   setRound(0);
+               }}
+          >上午</div>
+          <div className={round === 1 ? classes.roundSelected : classes.round}
+               onClick={event => {
+                   setRound(1);
+               }}
+          >下午</div>
         </div>
       </div>
     </div>
